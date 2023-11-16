450e08384c12f06538bbdc94d799523aaa4d278b
nathxrz <nathyrezendemachado@gmail.com>
2023-11-16 15:36:43 -0300

    Páginas de visualizar perfis

--- includes/components/head.php
+++ includes/components/head.php
@@ -8,8 +8,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo ($title) ?></title>
 
-    <link rel="stylesheet" href="./css/main.css">
-    <link rel="stylesheet" href="./css/<?php echo ($style) ?>.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/<?php echo ($style) ?>.css">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
